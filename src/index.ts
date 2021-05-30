6609982022eb381f60c84fa862c91b47b0736482
Frederik P. B. H <frepbh@gmail.com>
2021-05-30 04:23:12 +0200

    rename and more stuff

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,29 @@
-document.onload = () => {
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+function OnLoad() {
 	document.querySelectorAll(".input-box").forEach((x) => {
 		const elem = x as HTMLElement;
 		elem.innerText = elem.getAttribute("placeholder") ?? "";
 	});
-};
+}
 
-function OnBoxClick(elem: HTMLDivElement) {
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+function OnBoxFocus(elem: HTMLElement) {
+	if (elem.innerText != elem.getAttribute("placeholder")) return;
 	elem.innerText = "";
 }
 
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+function OnBoxBlur(elem: HTMLElement) {
+	if (elem.innerText != "") return;
+	elem.innerText = elem.getAttribute("placeholder") ?? "";
+}
+
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 function OnBodyClick(body: HTMLElement, event: MouseEvent) {
 	if (event.target != body) return;
+}
+
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+function OnSubmitClick() {
+	console.log("submit");
 }
